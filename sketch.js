f8bd89a8434ffbb71fdf4200c90ca0a1f58b5a43
Luís Gil <lupevibogil@gmail.com>
2021-02-17 15:10:05 +0000

    First initial commit

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
+let detailX;
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(100,100,WEBGL);
+  detailX = createSlider(3,24,3);
+  detailX.position(10,height+5);
+  detailX.style('width','80px');
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(25,123,255);  
+  rotateY(millis()/100);
+  sphere(40,detailX.value(),16);
 }
