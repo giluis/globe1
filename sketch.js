fb40ed4ff07c329bbb664a87eec380be503a77c3
macholatino45 <lupevibogil@gmail.com>
2021-02-17 17:35:42 +0000

    Add rotation and positioning to globe

--- sketch.js
+++ sketch.js
@@ -1,13 +1,44 @@
 let detailX;
+let img;
+const detail = 24;
+const radius = 100;
+let button;
+let autoRotate = false;
+let rotateCount = 0;
+let stopTime = 0;
+const cameraCoordinates = [
+  0,0,(height/2)/tan(PI/6),
+  0,0,0,
+  0,1,0
+];
+
+
 function setup() {
-  createCanvas(100,100,WEBGL);
+  createCanvas(800,800,WEBGL);
   detailX = createSlider(3,24,3);
   detailX.position(10,height+5);
   detailX.style('width','80px');
+  img = loadImage('map4.png');
+  button = createButton('click me');
+  button.position(19, 19);
+  button.mousePressed(e=>autoRotate = !autoRotate);
 }
 
+
+
+
 function draw() {
-  background(25,123,255);  
-  rotateY(millis()/100);
-  sphere(40,detailX.value(),16);
-}
+  // camera(...cameraCoordinates)
+  ambientLight(200);
+  background(0);
+  translate(-35,90,535)
+
+  if(autoRotate){
+    rotateY(rotateCount);
+    rotateCount++;
+  }  else
+    rotateY(rotateCount);
+  texture(img);
+  strokeWeight(0);
+  sphere(radius,detail,detail);
+};
