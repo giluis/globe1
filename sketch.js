d2ed5cf551bc1aa693d5f4192206c15bf08e3337
andresdavalosc <andres_el_davalos@hotmail.com>
2021-02-18 20:57:15 +0100

    added some animation

--- sketch.js
+++ sketch.js
@@ -6,14 +6,17 @@ let button;
 let autoRotate = false;
 let rotateCount = 0;
 let stopTime = 0;
-const cameraCoordinates = [
-  0,0,(height/2)/tan(PI/6),
-  0,0,0,
-  0,1,0
-];
+let bg;
+// const cameraCoordinates = [
+//   0,0,(height/2)/tan(PI/6),
+//   0,0,0,
+//   0,1,0
+// ];
+
 
 
 function setup() {
+  bg=loadImage('stars.jpg');
   createCanvas(800,800,WEBGL);
   detailX = createSlider(3,24,3);
   detailX.position(10,height+5);
@@ -22,13 +25,14 @@ function setup() {
   button = createButton('click me');
   button.position(19, 19);
   button.mousePressed(e=>autoRotate = !autoRotate);
+
 }
 
 
 
 
 function draw() {
-  // camera(...cameraCoordinates)
+  //camera(...cameraCoordinates)
   ambientLight(200);
   background(0);
   translate(-35,90,535)
