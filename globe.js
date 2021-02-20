362015e297993f8146b6bf1ede3505958041f6a2
macholatino45 <lupevibogil@gmail.com>
2021-02-20 16:31:53 +0000

    Add tag selection to globe

--- globe.js
+++ globe.js
@@ -1,119 +1,105 @@
+"use strict";
 class Globe {
-
-    static MAX_VEL = [0.03, 0.1, 0.1];
-    static MIN_VEL = [0, 0, 0]
-    static NORMAL_ACC = [0, 2e-4, 0];
-    static NORMAL_DESACC = [0, -3e-4, 0];
     constructor(radius, position, initialRotation, res) {
-        this.initialPosition = position;
-        let [rx, ry, rz] = initialRotation;
         this.pos = [...position];
+        this.rotation = [...initialRotation];
         this.radius = radius;
         this.rotationVel = [0, 0, 0];
-        this.rotation = [rx, ry, rz];
         this.rotationAcc = [0, 0, 0];
         this.res = res;
         this.isRotating = true;
         this.radians = true;
+        this.tags = [];
     }
     perimeter() {
-        return this.radius * 2 * PI;
+        return this.radius * 2 * Math.PI;
     }
-
     latVector(lat) {
         if (lat == 0)
             return [0, 0, 0];
         return [Math.sin(lat), 0, Math.cos(lat)];
     }
-
     lonVector(lon) {
         if (lon === 0)
-            return [0, 0, 0]
+            return [0, 0, 0];
         return [0, Math.sin(lon), Math.cos(lon)];
     }
-
     surfacePTranslation(lat, lon) {
         let la = lat ? this.latVector(lat) : [0, 0, 0];
         let lo = lon ? this.lonVector(lon) : [0, 0, 0];
         return [(la[0] + lo[0]), (la[1] + lo[1]), la[2] + lo[2]];
     }
-
-
-    markPoint(lat, lon) {
-        let translateVector = this.surfacePTranslation(lat, lon).map(c => c * this.radius);
-        let latVector = this.latVector(lat);
-        let lonVector = this.lonVector(lon);
-        push();
-        let z = (this.radius + 1) * Math.cos(lat + this.rotation[1]) * Math.cos(lon);
-        let x = (this.radius + 1) * Math.sin(lat + this.rotation[1]) * Math.cos(lon);
-        let y = (this.radius + 1) * Math.sin(lon);
-        
-        translate(...this.pos);
-        translate(x, y, z);
-        stroke(0, 255, 0);
-        strokeWeight(3);
-        fill(0, 0, 255);
-        plane(3)
-        pop()
-    }
-
-    halt() {
-        this.rotationVel = 0;
-    }
-
-    move() {
-        this.isRotating = [...Globe.MAX_VEL];
-    }
-
     start() {
         this.rotationAcc = [...Globe.NORMAL_ACC];
         this.isRotating = true;
     }
-
+    addTag(tWidth, tHeight, [lat, lon]) {
+        let t = new Tag(tWidth, tHeight, [lat, lon], this.pos, this.rotation, 24);
+        this.tags.push(t);
+    }
     stop() {
         this.rotationAcc = [...Globe.NORMAL_DESACC];
         this.isRotating = false;
     }
-
     toggleRotation() {
         if (!this.isRotating)
             this.start();
         else
             this.stop();
-
     }
     limitVel() {
         this.rotationVel = this.rotationVel.map((v, i) => {
             if (v > Globe.MAX_VEL[i]) {
                 this.rotationAcc[i] = 0;
                 return Globe.MAX_VEL[i];
-            } else if (v < Globe.MIN_VEL[i]) {
+            }
+            else if (v < Globe.MIN_VEL[i]) {
                 this.rotationAcc[i] = 0;
                 return Globe.MIN_VEL[i];
             }
             return v;
-        })
+        });
     }
+    //
     update() {
         this.limitVel();
         this.rotationVel = this.rotationVel.map((v, i) => v + this.rotationAcc[i]);
         this.rotation = this.rotation.map((r, i) => r + this.rotationVel[i]);
+        this.tags.forEach(t => t.update(this.radius, this.rotation));
     }
 
-
-
+    selectNextTag(){
+        let s;
+        for(let i = 0; i< this.tags.length;i++){
+            if(this.tags[i].selected){
+                if(i== this.tags.length-1)
+                    s = 0;
+                else
+                    s = i+1;
+                this.tags[i].selected = false;
+            }
+        }
+        if(!s)
+            this.tags[0].selected = true;
+        this.tags[s].selected = true;
+    }
 
     render() {
+
         push()
-        let [px, py, pz] = this.pos;
-        let [rx, ry, rz] = this.rotation;
-        translate(...this.pos);
+        let [rx, ry, rz] = globe.rotation;
+        translate(...globe.pos);
         rotateY(ry);
         rotateX(rx);
         rotateZ(rz);
         texture(img);
         noStroke();
-        sphere(this.radius, this.res, this.res);
+        sphere(globe.radius, globe.res, globe.res);
         pop();
+        globe.tags.forEach(t => t.render(this.pos));
     }
 }
+Globe.MAX_VEL = [0.03, 0.01, 0.1];
+Globe.MIN_VEL = [0, 0, 0];
+Globe.NORMAL_ACC = [0, 1e-4, 0];
+Globe.NORMAL_DESACC = [0, -3e-4, 0];
--- sketch.js
+++ sketch.js
@@ -19,12 +19,12 @@ let globe;
  * {Tag}
  */
 let tag1;
-let points= [
+let points = [
   [6.0012300000000005, 3.63922],
   [5.73703, 3.49077],
   [5.93844, 3.61422],
   [5.93844, 3.61422]
-  
+
 ];
 let latSlider;
 let lonSilder;
@@ -32,52 +32,47 @@ let latPreciseSlider;
 let lonPreciseSlider;
 let saveCoorsbtn;
 function setup() {
-  let cnv = createCanvas(700,windowHeight/2, WEBGL);
-  cnv.style("position:absolute;bottom:0;left:0");
-  bg=loadImage('stars.jpg');
-  createCanvas(800,800,WEBGL);
+  let cnv = createCanvas(800, 800, WEBGL);
+  cnv.style("position:absolute;top:0;left:0");
+  bg = loadImage('stars.jpg');
+  createCanvas(800, 800, WEBGL);
   img = loadImage('map4.png');
   face = loadImage('face1.png')
   button = createButton('Toggle rotation');
   button.position(19, 19);
   button.mousePressed(e => globe.toggleRotation());
-  globe = new Globe(radius, [0, 60, 0], [0.43,3.5,0], detail);
-  tag1 = new Tag(70,10,[0,0,radius+1],[0,3,0],24);
-  latSlider = createSlider(0,2*Math.PI,0,0.00001);
-  lonSlider = createSlider(0,2*Math.PI,0,0.00001);
-  latPreciseSlider = createSlider(0,0.1,0,0.001);
-  lonPreciseSlider = createSlider(0,0.1,0,0.001);
+  globe = new Globe(radius, [10, 40, 300], [0.43, 3.5, 0], detail);
+  latSlider = createSlider(0, 2 * Math.PI, 0, 0.00001);
+  lonSlider = createSlider(0, 2 * Math.PI, 0, 0.00001);
+  latPreciseSlider = createSlider(0, 0.1, 0, 0.001);
+  lonPreciseSlider = createSlider(0, 0.1, 0, 0.001);
   saveCoorsbtn = createButton('Save coors');
   saveCoorsbtn.mousePressed(e => saveCoors());
+  // globe.start();
+  points.forEach(p => globe.addTag(20, 30, p))
+  let nextBtn = createButton('Next tag');
+  nextBtn.mousePressed(e=>globe.selectNextTag());
 }
 
-  // tag1.startRotation();
+// tag1.startRotation();
 
 
-function saveCoors(){
-  points.push([latSlider.value()+latPreciseSlider.value(),lonSlider.value()+lonPreciseSlider.value()])
+function saveCoors() {
+  points.push([latSlider.value() + latPreciseSlider.value(), lonSlider.value() + lonPreciseSlider.value()])
 }
 
 
 function draw() {
   //camera(...cameraCoordinates)
+  background(0)
   ambientLight(200);
-  let camX = map(mouseX,0,width,-400,400);
-  let camZ = (height/2)/tan(PI/6);
-  let fov = PI/3;
-  perspective(fov,width/height,0,2000);
-  camera(0, 0, camZ -120, 0, 0,0, 0, 1, 0);
-  background(0);
-  point(10,10);
-  let coors = [latSlider.value()+latPreciseSlider.value(),lonSlider.value()+lonPreciseSlider.value()]
+  let camX = map(mouseX, 0, width, -400, 400);
+  let camZ = (height / 2) / tan(PI / 6);
+  let fov = PI / 3;
+  perspective(fov, width / height, 0, 2000);
+  camera(0, 0, camZ - 120, 0, 0, 0, 0, 1, 0);
+  let coors = [latSlider.value() + latPreciseSlider.value(), lonSlider.value() + lonPreciseSlider.value()]
   globe.update();
   globe.render();
-  points.forEach(([la,lo])=> globe.markPoint(la,lo));
-  globe.markPoint(...coors);
-  
-  // console.log(...coors);
-  // tag1.update();
-  // tag1.render();
 };
-
-// 6.11638 
+// 6.11638
