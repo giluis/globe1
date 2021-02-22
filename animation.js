5a5389e31452c83a39422d80f8bcbfddba41db65
macholatino45 <lupevibogil@gmail.com>
2021-02-22 00:34:54 +0000

    Add photos and correct coordinates

--- animation.js
+++ animation.js
@@ -1,71 +1,128 @@
-  var counter=-1;
-  var Name = ["Andres Davalos", "Luis Gil", "Vu Le Trong","Diego Pinto","Francesco Argentati","Laura Kruchen","Kevin Leriche"];
-  var study = ["Elektronic-ict", "IT", "IT","Sport science","Electronic Engineering","Medical Information Management","Sport science"];
-  var country = ["Belgium", "Portugal", "Germany","France","Italy","Germany","France"];
-  var ects = ["6", "18", "10","10","6","9","10"];
-  var contacts = ["andres_el_davalos@hotmail.com", "1170727@isep.it", "trongvul@hochsttrier.de","diego.pinto@etu.univ-orleans.fr","francescoargentati@libero.it","laura.kruchen@web.de","keleriche@gmail.com"];
-  var pic= ["andres.jpg","luis.jpg","vu.jpg","diego.jpg","francesco.jpg","laura.jpg","kevin.jpg"];
-  var cv = ["andres.pdf","luis.pdf","vu.pdf","diego.pdf","francesco.pdf","laura.pdf","kevin.pdf"];
-  var div = document.querySelector(".item");
-  var div2 = document.querySelector(".info");
+var counter = -1;
+var Name = [
+  "Luis Gil",
+  "Diego Pinto",
+  "Kevin Leriche",
+  "Andres Davalos",
+  "Laura Kruchen",
+  "Vu Le Trong",
+  "Francesco Argentati",
+];
+var study = [
+  "IT",
+  "Sport science",
+  "Sport science",
+  "Elektronic-ict",
+  "Medical Information Management",
+  "IT",
+  "Electronic Engineering",
+];
+var country = [
+  "Portugal",
+  "France",
+  "France",
+  "Belgium",
+  "Germany",
+  "Germany",
+  "Italy",
+];
+var ects = [
+  "18",
+  "10",
+  "10",
+  "6",
+  "9",
+  "10",
+  "6",
+];
+var contacts = [
+  "1170727@isep.it",
+  "diego.pinto@etu.univ-orleans.fr",
+  "keleriche@gmail.com",
+  "andres_el_davalos@hotmail.com",
+  "laura.kruchen@web.de",
+  "trongvul@hochsttrier.de",
+  "francescoargentati@libero.it",
+]
+  ;
+var pic = [
+  "luis.jpg",
+  "diego.jpg",
+  "kevin.jpg",
+  "andres.jpg",
+  "laura.jpg",
+  "vu.jpg",
+  "francesco.jpg",
+];
+var cv = [
+  "luis.pdf",
+  "diego.pdf",
+  "kevin.pdf",
+  "andres.pdf",
+  "laura.pdf",
+  "vu.pdf",
+  "francesco.pdf",
+];
+var div = document.querySelector(".item");
+var div2 = document.querySelector(".info");
+div.style.display = "none";
+div2.style.display = "none";
+var nextbtn = document.querySelector(".Next");
+var infobtn = document.querySelector(".Info");
+var prevbtn = document.querySelector(".Prev");
+let teamInfoText = "About the Team";
+let elementsInfoText = "About the People";
+let currentInfoText = elementsInfoText;
+function toggleInfoText() {
+  if (currentInfoText === teamInfoText) {
+    currentInfoText = elementsInfoText;
+  } else if (currentInfoText === elementsInfoText) {
+    currentInfoText = teamInfoText;
+  }
+}
+nextbtn.addEventListener("click", function () {
+  if (counter >= 6) {
+    counter = -1;
+  }
+  if (isGlobeMoving()) {
+    stopAtHome();
+  }
+  counter++;
+  selectNextTag();
+  updateinfo(counter, div, div2);
+});
+prevbtn.addEventListener("click", function () {
+  if (counter <= 0) {
+    counter = 7;
+  }
+  if (isGlobeMoving()) {
+    stopAtHome();
+  }
+  counter--;
+  selectPrevTag();
+  updateinfo(counter, div, div2)
+});
+infobtn.addEventListener("click", function () {
   div.style.display = "none";
-  div2.style.display = "none";
-  var nextbtn = document.querySelector(".Next");
-  var infobtn = document.querySelector(".Info");
-  var prevbtn = document.querySelector(".Prev");
-  let teamInfoText = "About the Team";
-  let elementsInfoText = "About the People"; 
-  let currentInfoText = elementsInfoText;
-  function toggleInfoText(){
-    if(currentInfoText === teamInfoText){
-      currentInfoText = elementsInfoText;
-    }else if(currentInfoText ===  elementsInfoText){
-      currentInfoText = teamInfoText;
-    }
-  }
-  nextbtn.addEventListener("click", function(){
-    if(counter >=6){
-      counter=-1;
-    }
-    if(isGlobeMoving()){
-      stopAtHome();
-    }
-    counter++;
-    selectNextTag();
-    updateinfo(counter,div,div2);
-  });
-  prevbtn.addEventListener("click", function(){
-    if(counter <=0){
-      counter=7;
-    }
-    if(isGlobeMoving()){
-      stopAtHome();
-    }
-    counter--;
-    selectPrevTag();
-    updateinfo(counter,div,div2)
-  });
-  infobtn.addEventListener("click", function(){
-    div.style.display="none";
-    div2.style.display = "block";
-    // toggleInfoText();
-    // let i = document.querySelector(".Info");
-    // i.innerHTML = currentInfoText;
-    startGlobe();
-    div.classList.add("fadeInRight");
-  });
+  div2.style.display = "block";
+  // toggleInfoText();
+  // let i = document.querySelector(".Info");
+  // i.innerHTML = currentInfoText;
+  startGlobe();
+  div.classList.add("fadeInRight");
+});
 
-  function updateinfo(count,d,d2){  
-    d2.style.display="none";
-    d.style.display = "block";
-    d.classList.add("fadeInRight");
-    document.getElementById("img").src = pic[count];
-    document.getElementById("Name").innerHTML = Name[count];
-    document.getElementById("country").innerHTML = country[count];
-    document.getElementById("study").innerHTML = study[count];
-    document.getElementById("ects").innerHTML = ects[count];
-    document.getElementById("contacts").innerHTML = contacts[count];
-    document.getElementById("cv").href = cv[count];
+function updateinfo(count, d, d2) {
+  d2.style.display = "none";
+  d.style.display = "block";
+  d.classList.add("fadeInRight");
+  document.getElementById("img").src = pic[count];
+  document.getElementById("Name").innerHTML = Name[count];
+  document.getElementById("country").innerHTML = country[count];
+  document.getElementById("study").innerHTML = study[count];
+  document.getElementById("ects").innerHTML = ects[count];
+  document.getElementById("contacts").innerHTML = contacts[count];
+  document.getElementById("cv").href = cv[count];
 
-    
-  }
+
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let detailX;
 let img;
 const detail = 40;
-const radius = 150;
+const radius = 167;
 let button;
 let autoRotate = false;
 let rotateCount = 0;
@@ -9,56 +9,84 @@ let stopTime = 0;
 let face;
 let bg;
 let globe;
-let points = [
-  [6.0012300000000005, 3.63922],
-  [5.73703, 3.49077],
-  [5.93844, 3.61422],
-  [5.93844, 3.61422]
+let latoffset = 6.0012300000000005;
+let lonoffset = 3.63922;
 
+let imagesSrc = [
+    "luis.jpg",
+    "diego.jpg",
+    "kevin.jpg",
+    "andres.jpg",
+    "laura.jpg",
+    "vu.jpg",
+    "francesco.jpg",
+]
+
+let images = [];
+let absolutePoints = [
+  [5.7212300000000065, 3.489220000000003],//Luis
+  [5.891230000000003, 3.579220000000001],//Diego
+  [5.921230000000002, 3.6092200000000005],//Leriche
+  [5.951230000000002, 3.6492199999999997],//Andres
+  [6.0012300000000005, 3.63922],//Laura
+  [5.991230000000001, 3.6692199999999993],//Vu
+  [6.01123, 3.489220000000003] // Francesco,
 ];
 
-let nextTagBtn;
+let absoluteHome = [0.43, 3.5, 0];
+let home = [0.43,3.5,0];
+let points = absolutePoints.map(p=>p);
+// let newPoint = [latoffset,lonoffset];
+// points.push(newPoint);
+
+function preload(){
+  images = imagesSrc.map(i=>loadImage(i));
+}
 
 function setup() {
   let cnv = createCanvas(900, 500, WEBGL);
   cnv.style("position:absolute;bottom:0;left:0");
   cnv.parent("globe");
-  bg = loadImage('stars.jpg');
   img = loadImage('map4.png');
   face = loadImage('face1.png')
-  globe = new Globe(radius, [12, 110, 190], [0.29, 3.5, 0], detail);
-  // nextTagBtn = createButton('Next Tag');
-  // nextTagBtn.position(19, 19);
-  // nextTagBtn.mousePressed(e => globe.selectNextTag());
+  globe = new Globe(radius, [12, 82, 190], home, detail);
+ 
+  saveCoorsBtn = createButton('Save coors');
+  saveCoorsBtn.mousePressed(e => saveCoors());
+  points.forEach((p,i)=>globe.addTag(20,30,p,images[i]));
+  images = imagesSrc.map(i=>loadImage(i));
+}
 
-  globe.start();
-  points.forEach(p => globe.addTag(20, 30, p))
+function keyPressed() {
+  if(keyCode === 37){
+    latoffset  += -0.01
+  } else if(keyCode === 38){
+    lonoffset  += 0.01;
+  } else if(keyCode === 39) {
+    latoffset  += 0.01;
+  } else if(keyCode === 40){
+    lonoffset  += -0.01;
+  }
 }
 
 
 function draw() {
   clear();
   ambientLight(200);
-  let camX = map(mouseX, 0, width, -400, 400);
-  let camZ = (height / 2) / tan(PI / 6);
-  let fov = PI / 3;
   globe.update();
   globe.render();
-  globe.markPoint(PI/2,-PI/2)
+  // globe.tags = [];
+  // points.forEach(p => {
+  //   globe.addTag(20, 30, p);
+  // })
+  // newPoint[0] = latoffset;
+  // newPoint[1] =lonoffset;
 };
 
 
 
 function saveCoors() {
-  points.push([latSlider.value() + latPreciseSlider.value(), lonSlider.value() + lonPreciseSlider.value()])
-}
-function createSliders() {
-  return {
-    latSlider: createSlider(0, 2 * Math.PI, 0, 0.00001),
-    lonSlider: createSlider(0, 2 * Math.PI, 0, 0.00001),
-    latPreciseSlider: createSlider(0, 0.1, 0, 0.001),
-    lonPreciseSlider: createSlider(0, 0.1, 0, 0.001)
-  }
+  points.push([])
 }
 
 function createButtons() {
@@ -66,17 +94,14 @@ function createButtons() {
   toggleBtn.position(19, 19);
   toggleBtn.mousePressed(e => globe.toggleRotation());
 
-  saveCoorsBtn = createButton('Save coors');
-  saveCoorsBtn.mousePressed(e => saveCoors());
-
-  return {toggleBtn,saveCoorsBtn}
+  return { toggleBtn, saveCoorsBtn }
 }
 function selectNextTag() {
   console.log("Inside slect next tag function")
   globe.selectNextTag();
 }
 
-function stopAtHome(){
+function stopAtHome() {
   globe.stopAtHome();
 }
 
@@ -88,9 +113,9 @@ function selectPrevTag() {
 function toggleRotation() {
   globe.toggleRotation();
 }
-function startGlobe(){
+function startGlobe() {
   globe.start();
 }
-function isGlobeMoving(){
-  return globe.rotationVel.filter(v=>v==0).length !== 0;
+function isGlobeMoving() {
+  return globe.rotationVel.filter(v => v == 0).length !== 0;
 }
